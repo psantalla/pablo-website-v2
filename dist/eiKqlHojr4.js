f08dcd937204700b37d1c53ebab8b8e37d01bdf6
psantalla <psantalla@users.noreply.github.com>
2025-12-08 04:54:06 +0000

    deploy: 319a23f06312b338f8850149b7af7656e8c8240c

--- dist/eiKqlHojr4.js
+++ dist/eiKqlHojr4.js
@@ -3,6 +3,7 @@
 			"@type": "BlogPosting",
 			"headline": "Time to switch",
 			"datePublished": "2025-07-17",
+			"dateModified": "2025-12-06",
 			"author": {
 				"@type": "Person",
 				"name": "Pablo Santalla"
