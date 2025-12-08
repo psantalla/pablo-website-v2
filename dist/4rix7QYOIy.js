7917c7ed5e18880befc57bd3b19beaba1f3fe0b3
psantalla <psantalla@users.noreply.github.com>
2025-12-08 17:20:04 +0000

    deploy: 1645aae1537db0ce801832efa0a55269ba162873

---
+++ dist/4rix7QYOIy.js
@@ -0,0 +1,10 @@
+{
+			"@context": "https://schema.org",
+			"@type": "BlogPosting",
+			"headline": "Procrastination deficit",
+			"datePublished": "2025-12-07",
+			"author": {
+				"@type": "Person",
+				"name": "Pablo Santalla"
+			}
+		}
