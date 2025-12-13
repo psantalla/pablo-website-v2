056e5446dab293ca6274b7e786d0c0c612db037c
psantalla <psantalla@users.noreply.github.com>
2025-12-13 07:13:46 +0000

    deploy: dbeaafbe942314a289cf71e27a50b5744833dfcb

---
+++ dist/9-ruJsSlc1.js
@@ -0,0 +1,10 @@
+{
+			"@context": "https://schema.org",
+			"@type": "BlogPosting",
+			"headline": "Mediocrity is a choice",
+			"datePublished": "2025-12-12",
+			"author": {
+				"@type": "Person",
+				"name": "Pablo Santalla"
+			}
+		}
