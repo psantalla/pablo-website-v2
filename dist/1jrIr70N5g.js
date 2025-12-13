a998338d61625c344e9a2368c66add97bb75abc0
psantalla <psantalla@users.noreply.github.com>
2025-12-13 06:12:17 +0000

    deploy: dd505fe0002eaf5c50f3c87a8ee4cc337e8a0511

---
+++ dist/1jrIr70N5g.js
@@ -0,0 +1,10 @@
+{
+			"@context": "https://schema.org",
+			"@type": "BlogPosting",
+			"headline": "Trying to impress your competence",
+			"datePublished": "2025-12-10",
+			"author": {
+				"@type": "Person",
+				"name": "Pablo Santalla"
+			}
+		}
