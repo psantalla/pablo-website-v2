0665874b34e27b46577dd687a63cbec5e3a4836c
psantalla <psantalla@users.noreply.github.com>
2025-12-15 06:31:17 +0000

    deploy: 081d87e5f9128ee5f6a3f92534f66d0ea75c29f6

---
+++ dist/4h04EWLT4G.js
@@ -0,0 +1,10 @@
+{
+			"@context": "https://schema.org",
+			"@type": "BlogPosting",
+			"headline": "Team solidarity",
+			"datePublished": "2025-12-14",
+			"author": {
+				"@type": "Person",
+				"name": "Pablo Santalla"
+			}
+		}
